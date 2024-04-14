9c7d671b43cda2d10991e65a44bcb9a8429de6df
Ravali08 <163737862+Ravali08@users.noreply.github.com>
2024-04-14 12:17:30 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,11 +1,14 @@
 node {
     stage('Checkout') {
-        git 'https://github.com/yourusername/yourrepository.git'
+        git branch: 'master', url: 'https://github.com/Ravali08/javalogin.git'
+    }
+     stage('Test') {
+        echo 'Testing in progress'
     }
     stage('Build') {
         sh 'mvn clean package'
     }
     stage('Deploy') {
-        sh 'cp target/*.war /path/to/tomcat/webapps/'
+        sh 'rsync -r target/*.war /etc/tomcat9/tomcat/webapps/"'
     }
 }
